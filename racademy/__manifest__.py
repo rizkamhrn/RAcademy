b020e89fd48e79897a70b01715f9e5b95813160d
rizkamhrn <99785464+rizkamhrn@users.noreply.github.com>
2022-04-05 08:56:12 +0700

    menghapus comment pada session_views dan session_report

--- racademy/__manifest__.py
+++ racademy/__manifest__.py
@@ -30,8 +30,8 @@
         'views/course_views.xml',
         'views/course_category_views.xml',
         'views/res_partner.xml',
-        # 'views/session_views.xml',
-        # 'report/session_report.xml'
+        'views/session_views.xml',
+        'report/session_report.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
